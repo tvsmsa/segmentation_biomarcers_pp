cfb925a221f4a73b45dc3d191b4541589047c566
tvsmsa <tmmelekhin@gmail.com>
2026-03-30 16:04:56 +0500

    Paths

--- ml/biomarcers/train_transunet.py
+++ ml/biomarcers/train_transunet.py
@@ -52,9 +52,9 @@ def dice_score_fast(preds, targets, ignore_index=config.IGNORE_INDEX):
     return dice_sum / count
 
 def train_fold(train_folds, val_fold, patience=5):
-    train_dfs = [pd.read_csv(f"D:\\aspirantura\\PROF\\npy_article_fold\\train_article_fold_{f}.csv") for f in train_folds]
+    train_dfs = [pd.read_csv(f"/kaggle/input/datasets/tvsmsa/aspirantura-biomarkers/aspirantura/PROF/npy_article_fold/train_article_fold_{f}.csv") for f in train_folds]
     df_train = pd.concat(train_dfs).reset_index(drop=True)
-    df_val = pd.read_csv(f"D:\\aspirantura\\PROF\\npy_article_fold\\train_article_fold_{val_fold}.csv")
+    df_val = pd.read_csv(f"/kaggle/input/datasets/tvsmsa/aspirantura-biomarkers/aspirantura/PROF/npy_article_fold/train_article_fold_{val_fold}.csv")
 
     train_dataset = ImageMaskDataset(df_train, augment_prob=0.5)
     val_dataset = ImageMaskDataset(df_val, augment_prob=0.0)
