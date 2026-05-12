85cedb4fbaba392e3935e7f5843b7978012c8489
tvsmsa <tmmelekhin@gmail.com>
2026-05-12 17:32:14 +0500

    Update train_transunet.py

--- ml/biomarcers/train_transunet.py
+++ ml/biomarcers/train_transunet.py
@@ -166,7 +166,7 @@ if __name__ == "__main__":
     torch.multiprocessing.set_start_method("spawn", force=True)
 
     # Задаём фолд вручную
-    FOLD = 2
+    FOLD = 3
 
     # Определяем train и val фолды
     if FOLD == 1:
