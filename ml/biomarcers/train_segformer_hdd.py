7d7347f1f946eff57e9dc17d3be3f7cd51d2285d
Алексей Мамаев <106877220+Alexey110945@users.noreply.github.com>
2026-05-21 20:00:08 +0500

    training segformer

--- ml/biomarcers/train_segformer_hdd.py
+++ ml/biomarcers/train_segformer_hdd.py
@@ -86,10 +86,16 @@ def train_fold(train_folds, val_fold, patience=5):
     # def combined_loss(logits, targets):
     #     return ce_loss(logits, targets) + 2.0 * tversky_loss(logits, targets)
 
-    focal_loss = FocalLoss(gamma=2.0).to(config.DEVICE)
+    focal_loss_fn = FocalLoss(gamma=2.0).to(config.DEVICE)
 
     def combined_loss(logits, targets):
-        return ce_loss(logits, targets) + 2.0 * focal_loss(logits, targets)
+        ce = ce_loss(logits, targets)
+        fl = focal_loss_fn(logits, targets)
+        # Если FocalLoss вернул 0 (без графа), добавляем фиктивный член
+        if fl.requires_grad:
+            return ce + 2.0 * fl
+        else:
+            return ce + 0.0 * logits.sum()
 
     scaler = torch.amp.GradScaler('cuda')
 
